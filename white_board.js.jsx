6eab19f7298b72818349f0e244a0b457e80ee2bc
Dwight Ware <dwightware4@gmail.com>
2015-10-15 16:03:51 -0700

    fixed bug with editing posts

--- white_board.js.jsx
+++ white_board.js.jsx
@@ -1,45 +1,22 @@
-<div className="jumbotron edit-jumbo">
-  <div className="row">
-    <div className="col-xs-4 col-xs-offset-4">
-      <h2 className="page-header">Edit Post</h2><br/>
-      <div className="form-group crazy-class-name">
-        <form onSubmit={this.editPost}>
+<div class="page-header">
+  <h1>Welcome Back to Vendrio!</h1>
+</div>
 
-          <label>Title:
-            <input className="form-control" type="text" name="title" placeholder={this.state.post.title}></input>
-          </label>
+<div class="row">
+  <div class="col-xs-4 col-xs-offset-4">
+    <form action="<%= session_url %>" method="post">
+      <%= auth_token %>
+      <div class="form-group">
+        <input class="form-control" type="text" name="user[username]" value="Guest">
 
-          <label>Description:
-            <input className="form-control" type="text" name="description" placeholder={this.state.post.description}></input>
-          </label>
+        <input class="form-control" type="password" name="user[password]" value="password">
 
-          <label>Price:
-            <input className="form-control" type="text" name="price" placeholder={this.state.post.price}></input>
-          </label>
-
-          <label>Lat:
-            <input className="form-control" type="text" name="latitude" placeholder={this.state.post.latitude}></input>
-          </label>
-
-          <label>Long:
-            <input className="form-control" type="text" name="longitude" placeholder={this.state.post.longitude}></input>
-          </label>
-
-          <label>Category:
-              <select className="btn btn-default dropdown-toggle" name="category">
-                <option value="-1">Select Category</option>
-                {
-                  this.state.categories.map(function(category){
-                    return <option value={category.id} selected={category.id === this.state.post.category_id ? 'selected' : false}>{category.title}</option>
-                  }.bind(this))
-                }
-              </select>
-            </label>
-
-          <input className="btn btn-default navbar-btn" type="submit" />
-
-        </form>
+        <input class="form-control btn btn-primary" type="submit" value="Sign In">
       </div>
+    </form><br>
+
+    <div>
+      <%= link_to "Sign Up", new_user_url %>
     </div>
   </div>
 </div>
