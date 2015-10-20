2b1172a101edf82c049fa3dace3ac43c41d3faa5
Dwight Ware <dwightware4@gmail.com>
2015-10-19 17:28:17 -0700

    fixed heroku?

--- app/assets/javascripts/components/posts/show.js.jsx
+++ app/assets/javascripts/components/posts/show.js.jsx
@@ -1,9 +1,12 @@
 window.PostShow = React.createClass({
   mixins: [ReactRouter.History],
   render: function(){
+    if(this.state.post === undefined) { return <div></div>; }
+
     return(
       <div className="jumbotron">
         <div className="row">
+
           <div className="col-xs-6">
             <h1 className="page-header">{this.state.post.title}</h1><br/>
             <p>Description: {this.state.post.description}</p><br/>
@@ -15,16 +18,54 @@ window.PostShow = React.createClass({
           </div>
 
           <div className="col-xs-6">
-            <img className="img-rounded" src={"http://res.cloudinary.com/vendrio/image/upload/c_fit,h_500,w_500/" + this.state.post.image}/>
+            <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
+              <ol className="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+              </ol>
+              <div className="carousel-inner" role="listbox">
+                <div className="carousel-item active">
+                  <img src="http://res.cloudinary.com/vendrio/image/upload/c_fit,h_500,w_500/v1445291500/pills_nnvwdl.jpg" />
+                </div>
+                <div className="carousel-item">
+                  <img src="http://res.cloudinary.com/vendrio/image/upload/c_fit,h_500,w_500/v1445291499/nikes_pfkqw6.jpg" />
+                </div>
+                <div className="carousel-item">
+                  <img src="http://res.cloudinary.com/vendrio/image/upload/c_fit,h_500,w_500/v1445291818/dream_spa_p58um7.jpg" />
+                </div>
+              </div>
+              <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span className="icon-prev" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+              </a>
+              <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span className="icon-next" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+              </a>
+            </div>
           </div>
-        </div>
 
+        </div>
       </div>
     );
   },
 
   getInitialState: function() {
     return {post: PostStore.find(parseInt(this.props.params.postId))};
+  },
+
+  _updateState: function() {
+    this.setState({post: PostStore.find(parseInt(this.props.params.postId))});
+  },
+
+  componentDidMount: function() {
+    PostStore.addChangeListener(this._updateState);
+    $('.carousel').carousel();
+  },
+
+  componentWillUnmount: function() {
+    PostStore.removeChangeListener(this._updateState);
   },
 
   deletePost: function(e) {
