0fb63644c3c495ec386aa1e39f5dd4d1d18c6e80
Dwight Ware <dwightware4@gmail.com>
2015-11-03 23:58:38 -0800

    new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,37 @@
-cat1 = Category.create(title: "Antiques")
-cat2 = Category.create(title: "Appliances")
-cat3 = Category.create(title: "Art")
-cat4 = Category.create(title: "Barter")
-cat5 = Category.create(title: "Bicycles")
-cat6 = Category.create(title: "Boats")
-cat7 = Category.create(title: "Books")
-cat8 = Category.create(title: "Business")
-cat9 = Category.create(title: "Cars")
-cat10 = Category.create(title: "Cellphones")
-cat11 = Category.create(title: "Clothing")
-cat12 = Category.create(title: "Collectibles")
-cat13 = Category.create(title: "Computers")
-cat14 = Category.create(title: "Electronics")
-cat15 = Category.create(title: "Free")
-cat16 = Category.create(title: "Furniture")
-cat17 = Category.create(title: "General")
-cat18 = Category.create(title: "Hoverboards")
-cat19 = Category.create(title: "Instruments")
-cat20 = Category.create(title: "Jewelry")
-cat21 = Category.create(title: "Materials")
-cat22 = Category.create(title: "Motorcycles")
-cat23 = Category.create(title: "Robots")
-cat24 = Category.create(title: "Sporting")
-cat25 = Category.create(title: "Tickets")
-cat26 = Category.create(title: "Tools")
-cat27 = Category.create(title: "Video Games")
-cat28 = Category.create(title: "Wanted")
+cat1 = Category.create(title: "Appliances")
+cat2 = Category.create(title: "Art")
+cat3 = Category.create(title: "Boats")
+cat4 = Category.create(title: "Cars")
+cat5 = Category.create(title: "Cellphones")
+cat6 = Category.create(title: "Clothing")
+cat7 = Category.create(title: "Collectibles")
+cat8 = Category.create(title: "Electronics")
+cat9 = Category.create(title: "Free")
+cat10 = Category.create(title: "Furniture")
+cat11 = Category.create(title: "General")
+cat12 = Category.create(title: "Hoverboards")
+cat13 = Category.create(title: "Instruments")
+cat14 = Category.create(title: "Jewelry")
+cat15 = Category.create(title: "Materials")
+cat16 = Category.create(title: "Motorcycles")
+cat17 = Category.create(title: "Robots")
+cat18 = Category.create(title: "Tickets")
+cat19 = Category.create(title: "Tools")
+cat20 = Category.create(title: "Video Games")
 
 user1 = User.create(username: "Dummy", password: "walnutcreek", email: "dummy@example.com", f_name: "Dummy", l_name: "User")
 user2 = User.create(username: "Guest", password: "password", email: "guest@example.com", f_name: "Guest", l_name: "User")
 
 post1 = Post.create(
-  title: "iPhone 7S",
-  description: "From 2017! Totally retro!",
+  title: "iPhone 11S",
+  description: "Brand new, never used!",
   longitude: rand(-122.501417..-122.387679),
   latitude: rand(37.648519..37.801154),
   city: "San Francisco",
   state: "CA",
-  price: 7000,
+  price: 700,
   user_id: user1.id,
-  category_id: cat10.id
+  category_id: cat5.id
 )
 
 post2 = Post.create(
@@ -49,9 +41,9 @@ post2 = Post.create(
   latitude: rand(37.648519..37.801154),
   city: "San Francisco",
   state: "CA",
-  price: 18000,
+  price: 4000,
   user_id: user1.id,
-  category_id: cat12.id
+  category_id: cat7.id
 )
 
 post3 = Post.create(
@@ -61,9 +53,9 @@ post3 = Post.create(
   latitude: rand(37.648519..37.801154),
   city: "San Francisco",
   state: "CA",
-  price: 1000000,
+  price: 125,
   user_id: user1.id,
-  category_id: cat12.id
+  category_id: cat7.id
 )
 
 post4 = Post.create(
@@ -73,9 +65,9 @@ post4 = Post.create(
   latitude: rand(37.648519..37.801154),
   city: "San Francisco",
   state: "CA",
-  price: 50000,
+  price: 17000,
   user_id: user1.id,
-  category_id: cat18.id
+  category_id: cat12.id
 )
 
 post5 = Post.create(
@@ -85,21 +77,21 @@ post5 = Post.create(
   latitude: rand(37.648519..37.801154),
   city: "San Francisco",
   state: "CA",
-  price: 50,
+  price: 40000,
   user_id: user1.id,
-  category_id: cat14.id
+  category_id: cat8.id
 )
 
 post6 = Post.create(
-  title: "2089 Honda ZHZ19",
+  title: "2048 Honda ZHZ19",
   description: "This frictionless drive motorcycle tops out at 297 KPH without any loss of power.",
   longitude: rand(-122.501417..-122.387679),
   latitude: rand(37.648519..37.801154),
   city: "San Francisco",
   state: "CA",
-  price: 80000,
+  price: 29000,
   user_id: user1.id,
-  category_id: cat22.id
+  category_id: cat16.id
 )
 
 post7 = Post.create(
@@ -109,9 +101,9 @@ post7 = Post.create(
   latitude: rand(37.648519..37.801154),
   city: "San Francisco",
   state: "CA",
-  price: 4000,
+  price: 175,
   user_id: user1.id,
-  category_id: cat11.id
+  category_id: cat6.id
 )
 
 post8 = Post.create(
@@ -123,23 +115,23 @@ post8 = Post.create(
   state: "CA",
   price: 2000,
   user_id: user1.id,
-  category_id: cat16.id
+  category_id: cat10.id
 )
 
 post9 = Post.create(
-  title: "Bieber Tickets",
-  description: "Tickets to the throwback Bieber World Tour, featuring a holographically rendered Justin Bieber and along with a long list of other famous historical artists.",
+  title: "Coffee Pot",
+  description: "Used Black and Decker coffee pot. Perfect condition, works great!",
   longitude: rand(-122.501417..-122.387679),
   latitude: rand(37.648519..37.801154),
   city: "San Francisco",
   state: "CA",
-  price: 0,
+  price: 60,
   user_id: user1.id,
-  category_id: cat24.id
+  category_id: cat18.id
 )
 
 post10 = Post.create(
-  title: "2086 Peugeot Speedster",
+  title: "2056 Audi AX7",
   description: "Perfect condition, fully refurbished every 3 months or 10,000 miles.",
   longitude: rand(-122.501417..-122.387679),
   latitude: rand(37.648519..37.801154),
@@ -147,7 +139,7 @@ post10 = Post.create(
   state: "CA",
   price: 100000,
   user_id: user1.id,
-  category_id: cat9.id
+  category_id: cat4.id
 )
 
 post11 = Post.create(
@@ -157,9 +149,9 @@ post11 = Post.create(
   latitude: rand(37.648519..37.801154),
   city: "San Francisco",
   state: "CA",
-  price: 3000,
+  price: 1500,
   user_id: user1.id,
-  category_id: cat14.id
+  category_id: cat1.id
 )
 
 post12 = Post.create(
@@ -169,13 +161,13 @@ post12 = Post.create(
   latitude: rand(37.648519..37.801154),
   city: "San Francisco",
   state: "CA",
-  price: 1500,
+  price: 195,
   user_id: user1.id,
-  category_id: cat11.id
+  category_id: cat6.id
 )
 
 post13 = Post.create(
-  title: "2027 Classic iMove",
+  title: "2029 Classic Apple iMove",
   description: "Great old iMove from the early days of autonomous cars.",
   longitude: rand(-122.501417..-122.387679),
   latitude: rand(37.648519..37.801154),
@@ -183,7 +175,7 @@ post13 = Post.create(
   state: "CA",
   price: 45000,
   user_id: user1.id,
-  category_id: cat9.id
+  category_id: cat4.id
 )
 
 post14 = Post.create(
@@ -195,7 +187,7 @@ post14 = Post.create(
   state: "CA",
   price: 200000,
   user_id: user1.id,
-  category_id: cat23.id
+  category_id: cat17.id
 )
 
 post15 = Post.create(
@@ -207,7 +199,7 @@ post15 = Post.create(
   state: "CA",
   price: 2000,
   user_id: user1.id,
-  category_id: cat17.id
+  category_id: cat11.id
 )
 
 post16 = Post.create(
@@ -217,9 +209,9 @@ post16 = Post.create(
   latitude: rand(37.648519..37.801154),
   city: "San Francisco",
   state: "CA",
-  price: 10000,
+  price: 2975,
   user_id: user1.id,
-  category_id: cat16.id
+  category_id: cat1.id
 )
 
 post17 = Post.create(
@@ -231,90 +223,187 @@ post17 = Post.create(
   state: "CA",
   price: 30000,
   user_id: user1.id,
-  category_id: cat16.id
+  category_id: cat10.id
 )
 
-img1 = Image.create(
-  url: "v1445291504/iphone_yj4d6n.jpg",
-  post_id: post1.id,
+post18 = Post.create(
+  title: "2058 Chevy Camaro",
+  description: "Beautiful condition. Fully loaded, low miles, folds up for easy storage.",
+  longitude: rand(-122.501417..-122.387679),
+  latitude: rand(37.648519..37.801154),
+  city: "San Francisco",
+  state: "CA",
+  price: 85000,
+  user_id: user1.id,
+  category_id: cat4.id
 )
 
-img2 = Image.create(
-  url: "v1445291498/laser_gun_qvswk3.jpg",
-  post_id: post2.id,
+post19 = Post.create(
+  title: "Weather proof hoodie",
+  description: "Barely worn, size large.",
+  longitude: rand(-122.501417..-122.387679),
+  latitude: rand(37.648519..37.801154),
+  city: "San Francisco",
+  state: "CA",
+  price: 75,
+  user_id: user1.id,
+  category_id: cat6.id
 )
 
-img3 = Image.create(
-  url: "v1445291503/teapot_vpl0zi.png",
-  post_id: post3.id,
+post20 = Post.create(
+  title: "Color-shifting heels",
+  description: "Brand new, changes color every 24 hours.",
+  longitude: rand(-122.501417..-122.387679),
+  latitude: rand(37.648519..37.801154),
+  city: "San Francisco",
+  state: "CA",
+  price: 250,
+  user_id: user1.id,
+  category_id: cat6.id
 )
 
-img4 = Image.create(
-  url: "v1445291500/hoverboard_oncb4d.jpg",
-  post_id: post4.id,
+post21 = Post.create(
+  title: "Sweet hat",
+  description: "Look so cool, such wow!",
+  longitude: rand(-122.501417..-122.387679),
+  latitude: rand(37.648519..37.801154),
+  city: "San Francisco",
+  state: "CA",
+  price: 1000000,
+  user_id: user1.id,
+  category_id: cat6.id
 )
 
-img5 = Image.create(
-  url: "v1445291500/time_machine_brfnsl.jpg",
-  post_id: post5.id,
+post22 = Post.create(
+  title: "Glow in the dark running shoes",
+  description: "Brand new, mens size 10.",
+  longitude: rand(-122.501417..-122.387679),
+  latitude: rand(37.648519..37.801154),
+  city: "San Francisco",
+  state: "CA",
+  price: 125,
+  user_id: user1.id,
+  category_id: cat6.id
 )
 
-img6 = Image.create(
-  url: "v1445291499/motorcycle_ancrnk.jpg",
-  post_id: post6.id,
+post23 = Post.create(
+  title: "Modern pants",
+  description: "What the kids wear these days",
+  longitude: rand(-122.501417..-122.387679),
+  latitude: rand(37.648519..37.801154),
+  city: "San Francisco",
+  state: "CA",
+  price: 79,
+  user_id: user1.id,
+  category_id: cat6.id
 )
 
-img7 = Image.create(
-  url: "v1445291502/shades_xvdou2.jpg",
-  post_id: post7.id,
+post24 = Post.create(
+  title: "Washer and Dryer",
+  description: "Used, in decent shape.",
+  longitude: rand(-122.501417..-122.387679),
+  latitude: rand(37.648519..37.801154),
+  city: "San Francisco",
+  state: "CA",
+  price: 750,
+  user_id: user1.id,
+  category_id: cat1.id
 )
 
-img8 = Image.create(
-  url: "v1445291817/couch_tesepc.jpg",
-  post_id: post8.id,
+post25 = Post.create(
+  title: "2 Slice Toaster",
+  description: "Brand new, great model!",
+  longitude: rand(-122.501417..-122.387679),
+  latitude: rand(37.648519..37.801154),
+  city: "San Francisco",
+  state: "CA",
+  price: 200,
+  user_id: user1.id,
+  category_id: cat1.id
 )
 
-img9 = Image.create(
-  url: "v1445291500/tickets_tppqih.jpg",
-  post_id: post9.id,
-)
+Image.create(url: "v1446621448/iphon2_dw8m0p.jpg", post_id: post1.id)
+Image.create(url: "v1446621448/iphone1_bohqhl.jpg", post_id: post1.id)
 
-img10 = Image.create(
-  url: "v1445291499/peugeot_tkelzv.jpg",
-  post_id: post10.id,
-)
+Image.create(url: "v1446621266/lasergun3_igbhgu.jpg", post_id: post2.id)
+Image.create(url: "v1446621266/lasergun2_qzegtv.jpg", post_id: post2.id)
+Image.create(url: "v1446621266/lasergun1_nmmzqc.jpg", post_id: post2.id)
 
-img11 = Image.create(
-  url: "v1445291818/dream_spa_p58um7.jpg",
-  post_id: post11.id,
-)
+Image.create(url: "v1445291503/teapot_vpl0zi.png", post_id: post3.id)
+Image.create(url: "v1446621147/hologram2_jjn1mt.jpg", post_id: post3.id)
 
-img12 = Image.create(
-  url: "v1445291499/nikes_pfkqw6.jpg",
-  post_id: post12.id,
-)
+Image.create(url: "v1446621033/hoverboard3_kz7d1h.jpg", post_id: post4.id)
+Image.create(url: "v1446621034/hoverboard2_x58mpi.jpg", post_id: post4.id)
+Image.create(url: "v1445291500/hoverboard_oncb4d.jpg", post_id: post4.id)
 
-img13 = Image.create(
-  url: "v1445291499/imove_ak1mki.jpg",
-  post_id: post13.id,
-)
+Image.create(url: "v1446620934/pod2_xbrqdm.jpg", post_id: post5.id)
+Image.create(url: "v1446620934/pod1_bpvw6g.jpg", post_id: post5.id)
 
-img14 = Image.create(
-  url: "v1445291499/robot_f3bq71.jpg",
-  post_id: post14.id,
-)
+Image.create(url: "v1446620771/motorcycle2_w4hojx.jpg", post_id: post6.id)
+Image.create(url: "v1446620771/motorcycle1_nlxwi2.jpg", post_id: post6.id)
 
-img15 = Image.create(
-  url: "v1445291500/pills_nnvwdl.jpg",
-  post_id: post15.id,
-)
+Image.create(url: "v1446620370/glasses2_bd8vyv.jpg", post_id: post7.id)
+Image.create(url: "v1446620371/glasses3_oh49bb.jpg", post_id: post7.id)
+Image.create(url: "v1446620375/glasses1_gfljag.jpg", post_id: post7.id)
 
-img16 = Image.create(
-  url: "v1445291500/shower_ebzih8.jpg",
-  post_id: post16.id,
-)
+Image.create(url: "v1446620257/couch2_dqwan5.jpg", post_id: post8.id)
+Image.create(url: "v1446620257/couch1_xv3z5a.jpg", post_id: post8.id)
 
-img17 = Image.create(
-  url: "v1445291818/bed_s8mvco.jpg",
-  post_id: post17.id,
-)
+Image.create(url: "v1446621822/coffeepot_odt1ou.jpg", post_id: post9.id)
+Image.create(url: "v1446621822/coffeepot2_v0sgdl.jpg", post_id: post9.id)
+
+Image.create(url: "v1446619973/audi1_gnsua6.jpg", post_id: post10.id)
+Image.create(url: "v1446619974/audi3_nsxpm5.jpg", post_id: post10.id)
+Image.create(url: "v1446619973/audi2_lt1sre.jpg", post_id: post10.id)
+
+Image.create(url: "v1446619770/dreamspa2_kpgybm.jpg", post_id: post11.id)
+Image.create(url: "v1445291818/dream_spa_p58um7.jpg", post_id: post11.id)
+Image.create(url: "v1446619771/dreamspa1_ubgzqt.jpg", post_id: post11.id)
+
+Image.create(url: "v1445291499/nikes_pfkqw6.jpg", post_id: post12.id)
+Image.create(url: "v1446619510/nikes3_trfwgz.jpg", post_id: post12.id)
+Image.create(url: "v1446619511/nikes2_vkf6hg.jpg", post_id: post12.id)
+
+Image.create(url: "v1446619355/imove3_odvuh5.jpg", post_id: post13.id)
+Image.create(url: "v1446619355/imove2_forkhw.jpg", post_id: post13.id)
+
+Image.create(url: "v1445291499/robot_f3bq71.jpg", post_id: post14.id)
+Image.create(url: "v1446619179/robot2_hu3dn9.jpg", post_id: post14.id)
+Image.create(url: "v1446619179/robot3_zxptpw.jpg", post_id: post14.id)
+
+Image.create(url: "v1445291500/pills_nnvwdl.jpg", post_id: post15.id)
+Image.create(url: "v1446619019/pills2_pfc6ck.jpg", post_id: post15.id)
+Image.create(url: "v1446619018/pills3_sbcrbo.jpg", post_id: post15.id)
+
+Image.create(url: "v1446618836/shower2_itgqut.jpg", post_id: post16.id)
+Image.create(url: "v1446618837/shower1_gyyzpi.jpg", post_id: post16.id)
+Image.create(url: "v1446618836/shower3_zq6daf.jpg", post_id: post16.id)
+
+Image.create(url: "v1446618044/sleeppod1_g6cjlm.jpg", post_id: post17.id)
+Image.create(url: "v1446618044/sleeppod3_f7ixwy.jpg", post_id: post17.id)
+Image.create(url: "v1446618044/sleeppod2_apt5oa.jpg", post_id: post17.id)
+
+Image.create(url: "v1446622117/camaro1_bkpyrl.jpg", post_id: post18.id)
+Image.create(url: "v1446622117/camara3_d4vrh5.jpg", post_id: post18.id)
+Image.create(url: "v1446622117/camaro_2_fuekgx.jpg", post_id: post18.id)
+
+Image.create(url: "v1446622361/hoodie_fnpqxc.jpg", post_id: post19.id)
+Image.create(url: "v1446622361/hoodie2_ia1euh.jpg", post_id: post19.id)
+
+Image.create(url: "v1446622541/heels1_ppwrre.jpg", post_id: post20.id)
+Image.create(url: "v1446622541/heels2_jdgv7h.jpg", post_id: post20.id)
+
+Image.create(url: "v1446622596/hat_ajbymw.jpg", post_id: post21.id)
+
+Image.create(url: "v1446622877/shoes2_shq4xn.jpg", post_id: post22.id)
+Image.create(url: "v1446622877/shoes3_y9ooqu.jpg", post_id: post22.id)
+Image.create(url: "v1446622877/shoes1_d3fzf9.jpg", post_id: post22.id)
+
+Image.create(url: "v1446622979/pants2_sqzejs.jpg", post_id: post23.id)
+Image.create(url: "v1446622979/pants1_etwdtl.jpg", post_id: post23.id)
+
+Image.create(url: "v1446623139/washer2_mkfmlk.jpg", post_id: post24.id)
+Image.create(url: "v1446623139/washer1_utyvw2.jpg", post_id: post24.id)
+
+Image.create(url: "v1446623579/toaster1_uhrjti.jpg", post_id: post25.id)
+Image.create(url: "v1446623579/toaster2_vmxohr.jpg", post_id: post25.id)
